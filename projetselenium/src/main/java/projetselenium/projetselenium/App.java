a4c0cc399afb02d07cc9982f6db981e3344db5bd
emmanuel10-PG <emmanuelpaul679@gmail.com>
2026-09-09 22:21:37 +0000

    test activite 2 avec Selenium Java pour exécuter le site www.youtube.com avec les navigateurs Firefox et Chrome.

--- projetselenium/src/main/java/projetselenium/projetselenium/App.java
+++ projetselenium/src/main/java/projetselenium/projetselenium/App.java
@@ -2,21 +2,32 @@ package projetselenium.projetselenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-/**
- * Hello world!
- *
- */
+
+
 public class App 
 {
     public static void main( String[] args )
     {
        
     	// Test automatisé ouverture d'une page avec Selenium
-        
+    	
+        // web dirver pour acceder au nivagateur Google Chrome
         System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.clubic.com");
+        
+        // Ouverture du navigateur Chrome puis accession au site web 
+        driver.get("https://www.youtube.com");
+        
+        // Web driver pour acceder au navigateur Firefox
+    	 System.setProperty("webdriver.gecko.driver", "./driver/geckodriver.exe");
+    	 WebDriverManager.firefoxdriver().setup();
+         WebDriver driverfirefox = new FirefoxDriver();	
+        
+        // Ouverture du navigateur Firefox puis accession au site web
+        driverfirefox.get("https://www.youtube.com");
         
         }
 }
