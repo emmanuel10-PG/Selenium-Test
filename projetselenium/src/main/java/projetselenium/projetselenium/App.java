5231e0f7a0d89001a41df021b92ac890157cb51f
emmanuel10-PG <emmanuelpaul679@gmail.com>
2026-09-11 00:49:45 +0000

    Selenium dimension et positionnement des pages

--- projetselenium/src/main/java/projetselenium/projetselenium/App.java
+++ projetselenium/src/main/java/projetselenium/projetselenium/App.java
@@ -4,6 +4,8 @@ package projetselenium.projetselenium;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -38,16 +40,15 @@ public class App
         
          
          // Temps d'attente 
-         
-        driverfirefox.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driverfirefox.manage().timeouts().scriptTimeout(Duration.ofSeconds(10));
-        driverfirefox.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-        
+//         
+//        driverfirefox.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+//        driverfirefox.manage().timeouts().scriptTimeout(Duration.ofSeconds(15));
+//        driverfirefox.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+//        
         // Ouverture du navigateur Firefox puis accession au site web
         driverfirefox.get("https://www.youtube.com");
 //    	  driverfirefox.findElement(By.cssSelector("ytd-button-renderer.ytd-consent-bump-v2-lightbox:nth-child(2) > yt-button-shape:nth-child(1) > button:nth-child(1)")).click();
         	
-        driverfirefox.navigate().to("https://www.clubic.com");
         
         // Web driver pour acceder au navigateur Microsoft Edge
    	    System.setProperty("webdriver.edge.driver", "./driver/msedgedriver.exe");
@@ -57,33 +58,49 @@ public class App
        // Ouverture du navigateur Microsoft Edge puis accession au site web
         DriverMedge.get("https://www.youtube.com");
         
-//        DriverMedge.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//        DriverMedge.manage().timeouts().scriptTimeout(Duration.ofSeconds(10));
-//        DriverMedge.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-        
-       // Autre méthode d'ouverture du navigateur Microsoft Edge puis accession au site web 
-       // Puisque j'ai mis youtube au debut ça rediriger vers www.clubic.com
-        DriverMedge.navigate().to("https://www.clubic.com");
+        DriverMedge.manage().timeouts().implicitlyWait(Duration.ofSeconds(11));
+        DriverMedge.manage().timeouts().scriptTimeout(Duration.ofMinutes(3));
+        DriverMedge.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(11));
+//        
         
         // reccuperer le titre d'un site web, on fait 
       //  DriverMedge.getTitle();
         
-        // permet de maximiser l'affichage de l'écran
-        DriverMedge.manage().window().maximize();
+//        // permet de maximiser l'affichage de l'écran
+//        DriverMedge.manage().window().maximize();
+//        
+//        // permet de minimiser l'affichage de l'écran
+//        DriverMedge.manage().window().minimize();
+//        
+//        // permet de plein écran l'affichage de l'écran
+//        DriverMedge.manage().window().fullscreen();
         
-        // permet de minimiser l'affichage de l'écran
-        DriverMedge.manage().window().minimize();
         
-        // permet de plein écran l'affichage de l'écran
-        DriverMedge.manage().window().fullscreen();
+        // effectuer les dimension
+        Dimension dims = DriverMedge.manage().window().getSize();
         
-        // fermer le navigateur
-        // pour une seule fenetre
-    //    DriverMedge.close();
+        DriverMedge.manage().window().setSize(new Dimension(600, 700));
         
-        // pour tout le navigateur pour plusieurs fenetre
-  //      DriverMedge.quit();
         
+        // Obtenir la dimension d'un site et l'afficher
+        DriverMedge.manage().window().getSize().getWidth();
+        DriverMedge.manage().window().getSize().getHeight();
+        
+        // Afficher la dimension d'un site sur la console
+        System.out.println(dims.getHeight());
+        System.out.println(dims.getWidth());
+        
+        // position d'une page web
+        Point positionPage = DriverMedge.manage().window().getPosition();
+        
+        System.out.println(positionPage.getX());
+        System.out.println(positionPage.getY());
+        
+        DriverMedge.manage().window().setPosition(new Point(700, 950));
+        
+       // Autre méthode d'ouverture du navigateur Microsoft Edge puis accession au site web 
+       // Puisque j'ai mis youtube au debut ça rediriger vers www.clubic.com
+        DriverMedge.navigate().to("https://www.clubic.com");
         
         // cliquer sur le bouton
 //        DriverMedge.findElement(By.cssSelector("ytd-button-renderer.ytd-consent-bump-v2-lightbox:nth-child(2) > yt-button-shape:nth-child(1) > button:nth-child(1)")).click();
