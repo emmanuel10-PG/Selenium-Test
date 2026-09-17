091a825700c5cdad9d10eea8ef0f43aec69d1547
emmanuel10-PG <emmanuelpaul679@gmail.com>
2026-09-17 21:10:55 +0000

    checkbox avec Selenium

--- projetselenium/src/main/java/projetselenium/projetselenium/App_activite.java
+++ projetselenium/src/main/java/projetselenium/projetselenium/App_activite.java
@@ -1,8 +1,11 @@
 package projetselenium.projetselenium;
 
+import java.util.List;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -12,25 +15,41 @@ public class App_activite {
 	public static void main(String[] args) {
 		
 		// Afficher une alerte au déclenchement d'un bouton 
+//		WebDriverManager.firefoxdriver().setup();
+//        WebDriver Driverfirefox = new FirefoxDriver();	
+//        Driverfirefox.get("https://the-internet.herokuapp.com/javascript_alerts");
+//        
+//        Driverfirefox.findElement(By.xpath("//button [@Onclick ='jsConfirm()']")).click();
+//        
+//        Alert alertJbtn = Driverfirefox.switchTo().alert();
+//        
+//        // obtenir le libele du bouton
+//        alertJbtn.getText();
+//         
+//        System.out.println("le libele du bouton est : " + alertJbtn.getText());	
+//        alertJbtn.accept();
+//        // si le bouton contien ce libele alors afficher le message dans la console
+//        if(Driverfirefox.getPageSource().contains("You clicked: Ok")) {
+//        	System.out.println("Very Good bon bouton");	
+//        }
+        
 		WebDriverManager.firefoxdriver().setup();
-        WebDriver Driverfirefox = new FirefoxDriver();	
-        Driverfirefox.get("https://the-internet.herokuapp.com/javascript_alerts");
+        WebDriver Driverfirefox = new FirefoxDriver();
+        Driverfirefox.get("https://the-internet.herokuapp.com/checkboxes");
         
-        Driverfirefox.findElement(By.xpath("//button [@Onclick ='jsConfirm()']")).click();
+        final int checkboxe_one = 0;
+        final int checkboxe_two = 1;
+        final By Selectorchk = By.cssSelector("[type ='checkbox']");
         
-        Alert alertJbtn = Driverfirefox.switchTo().alert();
+        // verification
+        List<WebElement> chkboxgroup = Driverfirefox.findElements(Selectorchk);
         
-        // obtenir le libele du bouton
-        alertJbtn.getText();
+        WebElement testElement1 = chkboxgroup.get((checkboxe_one));
+        WebElement testElement2 = chkboxgroup.get(checkboxe_two);
+        
+        System.out.print("\n checkbox numero 1 est " + testElement1.isSelected());
+        System.out.println("\n checkbox numero 2 est " + testElement2.isSelected());
          
-        System.out.println("le libele du bouton est : " + alertJbtn.getText());	
-        alertJbtn.accept();
-        // si le bouton contien ce libele alors afficher le message dans la console
-        if(Driverfirefox.getPageSource().contains("You clicked: Ok")) {
-        	System.out.println("Very Good bon bouton");	
-        }
-        
-        
 	}
 
 }
